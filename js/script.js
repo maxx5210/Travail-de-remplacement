e14948c39c9b523745110e73c1bedd7db2781cad
maxx5210 <thib.pierrot@hotmail.fr>
2020-05-29 15:24:26 +0200

    Collumn-reverse
    Co-Authored-By: null <calindore@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@ window.onload = function() {
   //Fonction qui permet de révéler la carte du dos du paquet de chaque joueur et de la placer à son emplacement
   var reveler = document.getElementById('dos1');
   reveler.onclick = function() {
+    console.log("Carte du joueur 1");
 
   }
 
   //Fonction qui permet de révéler la carte du dos du paquet de chaque joueur et de la placer à son emplacement
   var devoiler = document.getElementById('dos2');
   devoiler.onclick = function() {
+        console.log("Carte du joueur 2")
 
   }
 
