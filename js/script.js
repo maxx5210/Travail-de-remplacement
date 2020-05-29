94caf998bda28a0cf915308e0034f719b590b3ab
maxx5210 <thib.pierrot@hotmail.fr>
2020-05-29 15:16:42 +0200

    Affichage des dos de cartes
    Co-Authored-By: null <calindore@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 window.onload = function() {
 
   //Fonction qui permet de révéler la carte du dos du paquet de chaque joueur et de la placer à son emplacement
-  var reveler = document.getElementById('paquet1');
+  var reveler = document.getElementById('dos1');
   reveler.onclick = function() {
 
   }
 
   //Fonction qui permet de révéler la carte du dos du paquet de chaque joueur et de la placer à son emplacement
-  var devoiler = document.getElementById('paquet2');
+  var devoiler = document.getElementById('dos2');
   devoiler.onclick = function() {
 
   }
