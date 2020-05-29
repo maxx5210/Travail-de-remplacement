ce4b576ea3c920ed03182cae2e1b041279754e91
maxx5210 <thib.pierrot@hotmail.fr>
2020-05-29 15:14:10 +0200

    Mis à jour
    Co-Authored-By: null <calindore@users.noreply.github.com>

---
+++ js/script.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+
+  //Fonction qui permet de révéler la carte du dos du paquet de chaque joueur et de la placer à son emplacement
+  var reveler = document.getElementById('paquet1');
+  reveler.onclick = function() {
+
+  }
+
+  //Fonction qui permet de révéler la carte du dos du paquet de chaque joueur et de la placer à son emplacement
+  var devoiler = document.getElementById('paquet2');
+  devoiler.onclick = function() {
+
+  }
+
+
+
+
+
+
+
+
+
+}
