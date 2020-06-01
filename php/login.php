7046817bcc9aa98ef7b45cf6ac0bd21af3237124
maxx5210 <thib.pierrot@hotmail.fr>
2020-06-01 13:43:11 +0200

    Beautify du PHP

--- php/login.php
+++ php/login.php
@@ -1,19 +1,26 @@
 <?php
-include_once("config.php");
+include_once ("config.php");
 $user = $_POST['user'];
 $pass = $_POST['pass'];
 $pass = md5($pass);
 //Je devrais utilise password hash, je sais
+$bdd = new PDO('mysql:host=localhost;dbname=' . $db_name, $db_user, $db_pass, array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+)) or die('Failed to connect');
 
-$bdd = new PDO('mysql:host=localhost;dbname='.$db_name, $db_user, $db_pass, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)) or die('Failed to connect');
+$req = $bdd->prepare("SELECT * FROM users WHERE name = ? AND pass = ?") or die('Impossible d\'ajouter à la base');
+$req->execute(array(
+    $user,
+    $pass
+));
+$rep = $req->fetchAll();
 
-$req=$bdd->prepare("SELECT * FROM users WHERE name = ? AND pass = ?") or die('Impossible d\'ajouter à la base');
-$req->execute(array($user, $pass));
-$rep=$req->fetchAll();
-
-if(empty($rep)) {
+if (empty($rep))
+{
     echo "non";
-} else {
+}
+else
+{
     session_start();
     echo "oui";
 }
