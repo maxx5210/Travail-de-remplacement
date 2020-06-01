f72a30a7fadc71690d59360aa0bd18871244d5df
maxx5210 <thib.pierrot@hotmail.fr>
2020-06-01 17:33:28 +0200

    Divers
    Co-Authored-By: null <calindore@users.noreply.github.com>

--- js/bataille.js
+++ js/bataille.js
@@ -2,16 +2,19 @@ window.onload = function() {
 
   //Fonction qui permet de révéler la carte du dos du paquet de chaque joueur et de la placer à son emplacement
   var reveler = document.getElementById('dos1');
+  var carte1 = document.getElementById('carte1');
   reveler.onclick = function() {
     console.log("Carte du joueur 1");
+    carte1.src = "cartes/Jackhearts.svg";
 
   }
 
   //Fonction qui permet de révéler la carte du dos du paquet de chaque joueur et de la placer à son emplacement
   var devoiler = document.getElementById('dos2');
+  var carte2 = document.getElementById('carte2')
   devoiler.onclick = function() {
     console.log("Carte du joueur 2");
-
+    carte2.src = "cartes/8spades.svg";
   }
 
 
