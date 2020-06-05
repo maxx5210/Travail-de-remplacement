2333bf45e69ce748a46014dce37d423ade06920f
maxx5210 <thib.pierrot@hotmail.fr>
2020-06-05 14:08:57 +0200

    Essais de Sessions et animation des cartes

--- js/bataille.js
+++ js/bataille.js
@@ -12,6 +12,8 @@ var player1click;
 var player2click;
 var player1card;
 var player2card;
+var nbrcart1 = document.getElementById('cartpl1');
+var nbrcart2 = document.getElementById('cartpl2');
 var reserve = ["1clubs", "1diamonds", "1hearts", "1spades", "2clubs", "2diamonds", "2hearts", "2spades", "3clubs", "3diamonds", "3hearts", "3spades",
   "4clubs", "4diamonds", "4hearts", "4spades", "5clubs", "5diamonds", "5hearts", "5spades", "6clubs", "6diamonds", "6hearts", "6spades",
   "7clubs", "7diamonds", "7hearts", "7spades", "8clubs", "8diamonds", "8hearts", "8spades", "9clubs", "9diamonds", "9hearts", "9spades",
@@ -36,26 +38,41 @@ function comparaison() {
         console.log("Le J1 remporte les cartes");
         player1click = false;
         player2click = false;
-        reveler.classList.remove('disabled');
-        devoiler.classList.remove('disabled');
         for (let i = 0; i <= reserve.length - 1; i++) {
           paquet1.push(reserve[i]);
         }
         reserve.splice(0, reserve.length);
-        console.log("Paquet du J1 : ", paquet1);
+        carte1.classList.add('animationgauche');
+        carte2.classList.add('animationgauche');
+        setTimeout(function() {
+          carte1.src = "cartes/nothing.svg";
+          carte2.src = "cartes/nothing.svg";
+          reveler.classList.remove('disabled');
+          devoiler.classList.remove('disabled');
+          carte1.classList.remove('animationgauche');
+          carte2.classList.remove('animationgauche');
+        }, 1000);
+
         break;
 
       case player1card['valeur'] < player2card['valeur']:
         console.log("Le J2 remporte les cartes");
         player1click = false;
         player2click = false;
-        reveler.classList.remove('disabled');
-        devoiler.classList.remove('disabled');
         for (let i = 0; i <= reserve.length - 1; i++) {
           paquet2.push(reserve[i]);
         }
         reserve.splice(0, reserve.length);
-        console.log("Paquet du J2 : ", paquet2);
+        carte1.classList.add('animationdroite');
+        carte2.classList.add('animationdroite');
+        setTimeout(function() {
+          carte1.src = "cartes/nothing.svg";
+          carte2.src = "cartes/nothing.svg";
+          reveler.classList.remove('disabled');
+          devoiler.classList.remove('disabled');
+          carte1.classList.remove('animationdroite');
+          carte2.classList.remove('animationdroite');
+        }, 1000);
         break;
 
       case player1card['valeur'] === player2card['valeur']:
@@ -72,11 +89,14 @@ function comparaison() {
         break;
 
       default:
-        alert("Erreur FATALE ! (Comment ta fait ça Billy ? Sérieusement)")
+        alert("Erreur FATALE ! (Comment ta fait ça Billy ? Sérieusement ?)")
     }
   }
+  nbrcart1.innerHTML = "Nombre de cartes : " + paquet1.length;
+  nbrcart2.innerHTML = "Nombre de cartes : " + paquet2.length;
 }
 ///////////////////////////////////////////////////////////////////////////////
+
 
 ////////////////////////////////Pose des Cartes////////////////////////////////
 reveler.onclick = function() {
@@ -110,6 +130,7 @@ devoiler.onclick = function() {
 }
 ///////////////////////////////////////////////////////////////////////////////
 
+
 ////////////////////////////////Mélange du paquet//////////////////////////////
 function distribution() {
   for (let i = 0; i <= 51; i++) {
@@ -134,7 +155,6 @@ function distribution() {
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
-
 
 ///////////////////////////////////////////////////////////////////////////////
 
---
+++ js/session.js
@@ -0,0 +1,40 @@
+var obj;
+window.onload = function() {
+  get();
+}
+
+
+function createHttpRequest() {
+  var req = null;
+  if (window.XMLHttpRequest) {
+    req = new XMLHttpRequest();
+  } else {
+    try {
+      req = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+      try {
+        req = new ActiveXObject("Microsoft.XMLHTTP");
+      } catch (e2) {}
+    }
+  }
+  if (req == null) {
+    alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
+  }
+  return req;
+}
+
+function get() {
+  var req = createHttpRequest();
+  req.onreadystatechange = function() {
+    if (req.status == 200) {
+      console.log(req.status);
+      obj = req.responseText;
+    }
+  }
+  req.open("POST", "php/test.php", true);
+  req.send();
+  setTimeout(function() {
+    document.getElementById('result').innerHTML = obj;
+  }, 1000);
+
+}
